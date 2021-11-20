74b8c746fc8b7f1ad582dbe637406be399323ab2
Vyacheslav <dyaditsyn@gmail.com>
2021-11-20 08:29:10 +0200

    added work and education personal info

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,8 +51,14 @@ sr.reveal('.skills__text',{});
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+/*SCROLL EXPERIENCE*/
+sr.reveal('.experience__img',{}); 
+sr.reveal('.experience__year',{interval: 200}); 
+sr.reveal('.experience__list',{interval: 400}); 
+
+/*SCROLL EDUCATION*/
+sr.reveal('.education__img',{}); 
+sr.reveal('.education__year',{interval: 200}); 
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
